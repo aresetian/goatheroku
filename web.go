5ca06a284bffc1929a87e051dd4461a6bc8cd398
Carlos Andrés García García <carlosandresgg@gmail.com>
2014-03-12 12:49:14 -0400

    dependencias

--- web.go
+++ web.go
@@ -37,21 +37,21 @@ func hello(res http.ResponseWriter, req *http.Request) {
     b1 := memory.AccessString(m1);
     m1 = nil
     fmt.Fprintln(res, b1)
-    
+    /*
     fmt.Fprintln(res, "1000000 structuras")
     m2 , a2 := memory.InsertString(1000000)
     fmt.Fprintln(res, a2)
     b2 := memory.AccessString(m2);
     m2 = nil
     fmt.Fprintln(res, b2)
-    /*
+    
     fmt.Fprintln(res, "5000000 structuras")
     m3 , a3 := memory.InsertString(50000000)
     fmt.Fprintln(res, a3)
     b3 := memory.AccessString(m3);
     m3 = nil
     fmt.Fprintln(res, b3)
-    
+    */
     fmt.Fprintln(res, "<h2>Prueba  de carga con mapas de Int</h2>")
     fmt.Fprintln(res, "100000 structuras")
     m4 , a4 := memory.InsertInt(100000)
@@ -66,7 +66,7 @@ func hello(res http.ResponseWriter, req *http.Request) {
     b5 := memory.AccessInt(m5);
     m5 = nil
     fmt.Fprintln(res, b5)
-    
+    /*
     fmt.Fprintln(res, "1000000 structuras")
     m6 , a6 := memory.InsertInt(1000000)
     fmt.Fprintln(res, a6)
